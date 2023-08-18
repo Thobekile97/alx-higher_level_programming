ec6184197619b454fdbe6e4410be7d26fe75d349
Thobekile97 <tndlovi56@gmail.com>
2023-08-18 03:16:26 -0700

    7-update_dictionary.py

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def modify_dictionary(input_dict, new_key, new_value):
+    """Update or insert key/value pairs in a dictionary."""
+    input_dict[new_key] = new_value
+    return input_dict
+
